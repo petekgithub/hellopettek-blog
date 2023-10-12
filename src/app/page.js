fcd32ba2adcd1aac48ee917a4da57d433b4ba839
Petech <petekshamitbeyli@gmail.com>
2023-10-12 12:56:16 +0300

    css-responsive-layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,31 @@ import Link from "next/Link";
 export default function Home() {
   return (
     <div>
-      <Link href="/">Hello</Link>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+          dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+          amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem
+          ipsum
+        </h1>
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet.
+      </Link>
     </div>
   );
 }
