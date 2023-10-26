578ba08544e59d347fa75c71a80836aa75397ec2
Petech <petekshamitbeyli@gmail.com>
2023-10-27 00:04:57 +0300

    css-flexbox-update

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,8 @@ import AuthProvider from "@/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "hellopettekBlog",
-  description: "Created by hellopettek!",
+  title: "HelloPettek Blog",
+  description: "The best blog app!",
 };
 
 export default function RootLayout({ children }) {
