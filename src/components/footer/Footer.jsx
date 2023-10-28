d32fb0e51a6b3fef93a840d3596d100154c52b7e
Petech <petekshamitbeyli@gmail.com>
2023-10-29 00:11:20 +0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image
+            src="/hellopettek.png"
+            alt="hellopettek blog"
+            width={50}
+            height={50}
+          />
+          <h1 className={styles.logoText}>HelloPettek Blog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum
+          dolor sit amet. Lorem ipsum dolor sit amet.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/twitter.png" alt="" width={18} height={18} />
+          <Image src="/medium.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Twitter</Link>
+          <Link href="/">Medium</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
