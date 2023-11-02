3f206a7ade955ce4121f8627a7e0ab606fd930f8
Petech <petekshamitbeyli@gmail.com>
2023-11-03 02:26:03 +0300

    authjs

---
+++ src/app/api/[...nextauth]/route.js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+});
