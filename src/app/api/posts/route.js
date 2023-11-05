d24b6f669cad3292c2c0b79a7ce3da009f956e1f
Petech <petekshamitbeyli@gmail.com>
2023-11-05 03:39:58 +0300

    rest-api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -8,12 +8,17 @@ export const GET = async (req) => {
 
   const POST_PER_PAGE = 2;
 
+  const query = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (page - 1),
+  };
+
   try {
-    const posts = await prisma.post.findMany({
-      take: POST_PER_PAGE,
-      skip: POST_PER_PAGE * (page - 1),
-    });
-    return new NextResponse(JSON.stringify(posts, { status: 200 }));
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany(query),
+      prisma.post.count(),
+    ]);
+    return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
   } catch (error) {
     console.log(error);
     return new NextResponse(
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,9 +3,9 @@ import styles from "./card.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
         <Image src="/p2.jpeg" alt="" fill className={styles.image} />
       </div>
@@ -15,7 +15,7 @@ const Card = () => {
           <span className={styles.category}>SPORT</span>
         </div>
         <Link href="/">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <h1>{item.title}</h1>
         </Link>
         <p className={styles.desc}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum
