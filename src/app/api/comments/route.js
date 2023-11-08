ae86774d98a32dd40dfde51d36b86297de969551
Petech <petekshamitbeyli@gmail.com>
2023-11-08 12:29:32 +0300

    nextjs-swr-mutation

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,7 +1,8 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-//GET ALL COMMENTS OF A POST
+// GET ALL COMMENTS OF A POST
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
   // console.log(searchParams);
@@ -16,6 +17,31 @@ export const GET = async (req) => {
     });
 
     return new NextResponse(JSON.stringify(comments, { status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
+
+// CREATE A COMMENT
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
+
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    });
+
+    return new NextResponse(JSON.stringify(comment, { status: 200 }));
   } catch (error) {
     console.log(error);
     return new NextResponse(
